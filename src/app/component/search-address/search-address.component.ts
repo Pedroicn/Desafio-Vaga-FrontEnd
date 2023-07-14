0cedeac609c754f1c1438282b73822aea9bccafc
Pedroicn <pedro.icn88@gmail.com>
2023-07-14 11:25:05 -0300

    feat: implements validation logic

--- src/app/component/search-address/search-address.component.ts
+++ src/app/component/search-address/search-address.component.ts
@@ -8,11 +8,14 @@ import { AddressService } from '../../services/address.service';
 })
 export class SearchAddressComponent {
   cep: string = '';
+  validateCep: RegExp = /^[0-9]{8}$/;
   street: string;
   neighborhood: string;
   city: string;
   state: string;
   IBGE: string;
+  error: string;
+  isCepValid: boolean = false;
 
   constructor(private addressService: AddressService) {
     this.street = '';
@@ -20,16 +23,34 @@ export class SearchAddressComponent {
     this.city = '';
     this.state = '';
     this.IBGE = '';
+    this.error = '';
   }
 
   getData(): void {
-    this.addressService.fetchData(this.cep).subscribe((response) => {
-      this.street = response.logradouro;
-      this.neighborhood = response.bairro;
-      this.city = response.localidade;
-      this.state = response.uf;
-      this.IBGE = response.ibge;
-    });
+    if (this.validateCep.test(this.cep))
+      this.addressService.fetchData(this.cep).subscribe((response) => {
+        if (response.erro) {
+          this.isCepValid = true;
+          this.error = 'Digite um cep válido';
+        }
+        this.isCepValid = false;
+        this.street = response.logradouro;
+        this.neighborhood = response.bairro;
+        this.city = response.localidade;
+        this.state = response.uf;
+        this.IBGE = response.ibge;
+        console.log(response);
+      });
+    else {
+      this.isCepValid = true;
+      this.error = 'Digite um cep válido';
+    }
+  }
+
+  cepValidation() {
+    if (this.cep.length === 8) {
+      this.getData();
+    }
   }
 
   isButtonDisabled() {
@@ -38,7 +59,8 @@ export class SearchAddressComponent {
       this.neighborhood === '' ||
       this.city === '' ||
       this.state === '' ||
-      this.IBGE === ''
+      this.IBGE === '' ||
+      this.cep.length < 8
     ) {
       return true;
     }
